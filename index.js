9145f6eb8f44bdeeff161ca0b7a4e6fdec2c858c
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2023-03-27 16:17:00 -0300

    Created first slash command (27/03)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Require the necessary discord.js classes ---- 36:08 minutos
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 
 // Criando dotenv com require
 const dotenv = require('dotenv');
@@ -9,31 +9,47 @@ const { TOKEN, CLIENT_ID, GUILD_ID } = process.env
 //Importação dos comandos
 const fs = require("node:fs");
 const path = require('node:path');
-
-
 const commandsPath = path.join(__dirname, "commands")
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
 
-for (const file of commandFiles){
-	const filePath = path.join(commandsPath, file)
-	const commands = require(filePath)
-	console.log(commands)
-	// // Set a new item in the Collection with the key as the command name and the value as the exported module
-	// if ('data' in slash_commands && 'execute' in slash_commands) {
-	// 	client.slash_commands.set(slash_commands.data.name, slash_commands);
-	// } else {
-	// 	console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	// }
-
-}
-
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection()
+
+
+for (const file of commandFiles){
+	const filePath = path.join(commandsPath, file)
+	const command = require(filePath)
+	// // Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+console.log(client.commands)
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
 	console.log(`Pronto! Login realizado como ${c.user.tag}`);
 });
-
 client.login(TOKEN)
+
+//Listenner de interação
+client.on(Events.InteractionCreate, async interaction => {
+	if(!interaction.isChatInputCommand()) return
+	const command = interaction.client.commands.get(interaction.commandName)
+	if (!command){
+		console.error("Comando não encontrado")
+		return
+	}
+	try{
+		await command.execute(interaction)
+	}
+	catch(error){
+		console.error(error)
+		await interaction.reply("Ops... Houve um erro ao executar esse comando!")
+	}
+})
